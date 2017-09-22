d866f2935d9f8bd7c89ab208e478156b2fd32508
Paolo Gervasoni <pgvila@hotmail.com>
2017-09-22 22:28:29 +0200

    1st commit after merge master-feat2

--- f1.py
+++ f1.py
@@ -1,8 +1,12 @@
 import numpy
 import pandas as pd
+import networkx as nx
 for i in range(10):
     print(i)
 [x**2 for x in range(3)]
 
 # feature 2
 {k:l for k,l in zip(range(10), range(10))}
+
+g = nx.Graph()
+g.add_node('Paolo')
